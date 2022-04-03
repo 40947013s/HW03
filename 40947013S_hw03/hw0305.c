704a33cd16b3645cee61d027e7a49106fc63fa64
40947013S <100991325+40947013s@users.noreply.github.com>
2022-04-03 16:59:17 +0000

    hw0305

--- 40947013S_hw03/hw0305.c
+++ 40947013S_hw03/hw0305.c
@@ -1,10 +1,13 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define exit_program printf("Invaild input.\n"),\
+#define exit_program printf("Invaild input.\nExit\n"),\
+exit(0);
+#define wrong_file printf("File could not be opened!\nExit\n"),\
 exit(0);
 
 typedef struct _sBmpheader
@@ -32,23 +35,15 @@ void counter(FILE *step, int *m, int *n, int *count)
     return;
 }
 
-void printer(int **cube, int n, int m, int count)
-{
-    /*for(int i = 1; i < count+1; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k < m; k++)
-            {
-                printf("%2d ", cube[i][j*m+k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }*/
+bool check(int a, int b, int m, int n)
+{  
+    int x = abs((a-1)/m-(b-1)/m), y = abs((a-1)%m-(b-1)%m);
+    printf("%d %d %d %d\n", a, b, x, y);
+    if((x != 0 && x != 1) || (y != 0 && y != 1) || (x^y) == 0) return false;
+    return true;
 }
 
-bool check(FILE *step, FILE *pFile)
+void puzzle(FILE *step, FILE *pFile)
 {
     int m, n, count, pivot;
     char line[100];
@@ -56,13 +51,15 @@ bool check(FILE *step, FILE *pFile)
     
     counter(step, &m, &n, &count);
     fgets(line, 100, step);
-    int cube[count+1][n*m];
+    int cube[count+1][n*m], change[count+1][2];
     bool used[n*m+1];
     for(int i = 0; i < n*m; i++)
     {
         cube[0][i] = i+1;
         used[i] = false;
     } used[n*m] = false;
+    for(int i = 0; i < count+1; i++)
+        change[i][0] = -1, change[i][1] = 0;
     for(int i = 0; i < n*m; i++)
     {
         fscanf(step, "%d", &cube[1][i]);
@@ -71,8 +68,7 @@ bool check(FILE *step, FILE *pFile)
         used[cube[1][i]] = true;
         if(i % n == n-1) fgets(line, 100, step);
     }
-    if(used[0] == false)
-        exit_program;
+    if(used[0] == false) exit_program;
     for(int i = 0; i < n*m+1; i++)
     {
         if(used[i] == false) pivot = i;
@@ -81,58 +77,92 @@ bool check(FILE *step, FILE *pFile)
     
     for(int i = 2; i < count+1; i++)
     {
-        for(int j = 0; j < n*m; j++)
+        for(int j = 0, c = 0; j < n*m; j++)
         {
             fscanf(step, "%d", &cube[i][j]);
             if(used[cube[i][j]] == true || cube[i][j] == pivot)
                 exit_program;
+            if(cube[i-1][j] != cube[i][j])
+                change[i][c++] = j+1;
             used[cube[i][j]] = true;
         }
+    
+        if(cube[i][change[i][0]-1] != 0 && cube[i][change[i][1]-1] != 0)
+            exit_program;
+        if(!check(change[i][0], change[i][1], m, n))
+            exit_program;
         for(int j = 0; j < n*m+1; j++)
             used[j] = false;        
     }
-
-    for(int i = 1; i < count+1; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            for(int k = 0; k < m; k++)
-            {
-                printf("%2d ", cube[i][j*m+k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-
-    FILE *pFile2 = fopen("1.bmp", "wr");
+    
+    FILE *pFile2 = fopen("1.bmp", "w");
     fread(&header, 1, sizeof(Bmpheader), pFile);
     fwrite(&header, 1, sizeof(Bmpheader), pFile2);
     int in_size = ((header.width*header.bpp+31) >> 5) << 2; 
-    uint8_t *in_color = (uint8_t *)malloc(sizeof(uint8_t) *in_size);
-    int h = header.height/n, v = header.width/m;
+    uint8_t *in_color = (uint8_t *)malloc(header.height *in_size);
+    int h = header.height/n, v = header.width/m, space = 0;
+    fread(in_color, header.height, in_size, pFile);
+    
     for(int i = 0; i < header.height; i++)
     {
-        fread(in_color, 1, in_size, pFile);
         if(i % h == 0 || (i-1) % h == 0 || (i+1) % h == 0)
-        {
-            memset(in_color, 0, in_size);
-            fwrite(in_color, 1, in_size, pFile2);
-            continue;
-        }
-        for(int j = 0; j < in_size; j+=3)
-        {
-            if(j/3 % v == 0 || (j/3-1) % v == 0 || (j/3+1) % v == 0)  
-            {
+            for(int j = 0; j < in_size; j+=3)
                 for(int k = 0; k < 3; k++)
-                {
-                    in_color[j+k] = 0;
-                }
-            }
-        }
-        fwrite(in_color, 1, in_size, pFile2);
+                    in_color[i*in_size+j+k] = 0;
+        else
+            for(int j = 0; j < in_size; j+=3)
+                if(j/3 % v == 0 || (j/3-1) % v == 0 || (j/3+1) % v == 0)
+                    for(int k = 0; k < 3; k++)
+                        in_color[i*in_size+j+k] = 0;
+                else if((header.height-i)/h == (pivot-1)/n && (j/3)/v == (pivot-1)%n)
+                    for(int k = 0; k < 3; k++)
+                        in_color[i*in_size+j+k] = 255, space++;
     }
+    fwrite(in_color, header.height, in_size, pFile2);
     
+    for(int l = 2; l < count+1; l++)
+    {
+        char old_name[255], new_name[255];
+        sprintf(old_name, "%d.bmp", l-1);
+        sprintf(new_name, "%d.bmp", l);
+        if((pFile = fopen(old_name, "r")) == NULL) wrong_file;
+        if((pFile2 = fopen(new_name, "w")) == NULL) wrong_file;
+        fread(&header, 1, sizeof(Bmpheader), pFile);
+        fwrite(&header, 1, sizeof(Bmpheader), pFile2);
+        fread(in_color, header.height, in_size, pFile);
+        uint8_t *color = (uint8_t *)malloc(space);
+        int count_space = 0, white;
+        pivot = cube[l][change[l][0]-1] == 0 ? change[l][1] : change[l][0];
+        white = cube[l][change[l][0]-1] == 0 ? change[l][0] : change[l][1];
+        for(int i = 0; i < header.height; i++)
+        {
+            if(i % h == 0 || (i-1) % h == 0 || (i+1) % h == 0) continue;
+            else
+                for(int j = 0; j < in_size; j+=3)
+                    if(j/3 % v == 0 || (j/3-1) % v == 0 || (j/3+1) % v == 0)
+                        continue;
+                    else if((header.height-i)/h == (white-1)/n && (j/3)/v == (white-1)%n)
+                        for(int k = 0; k < 3; k++)
+                        {
+                            color[count_space++] = in_color[i*in_size+j+k];
+                            in_color[i*in_size+j+k] = 255;
+                        }                
+        }
+        count_space = 0;
+        for(int i = 0; i < header.height; i++)
+        {
+            if(i % h == 0 || (i-1) % h == 0 || (i+1) % h == 0)
+                continue;
+            else
+                for(int j = 0; j < in_size; j+=3)
+                    if(j/3 % v == 0 || (j/3-1) % v == 0 || (j/3+1) % v == 0)
+                        continue;
+                    else if((header.height-i)/h == (pivot-1)/n && (j/3)/v == (pivot-1)%n)
+                        for(int k = 0, space = 0; k < 3; k++)
+                            in_color[i*in_size+j+k] = color[count_space++];              
+        }
+        fwrite(in_color, header.height, in_size, pFile2);         
+    }
 }
 
 int main()
@@ -146,11 +176,7 @@ int main()
         input[strlen(input) - 1] = 0;
     
     if((bmp_in = fopen(input, "r")) == NULL)
-    {
-        printf("File could not be opened!\n");
-        return 0;
-    }
-
+        wrong_file;
 
     printf("Please enter the puzzle: ");
     fgets(step_name, 255, stdin);
@@ -158,9 +184,8 @@ int main()
         step_name[strlen(step_name) - 1] = 0;
     
     if((step = fopen(step_name, "r")) == NULL)
-    {
-        printf("File could not be opened!\n");
-        return 0;
-    }
-    check(step, bmp_in);
+        wrong_file;
+    puzzle(step, bmp_in);
+    fclose(step);
+    fclose(bmp_in);
 }
